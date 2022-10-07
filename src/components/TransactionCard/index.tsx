097d9b5656d4091843d0c638784896754a27c4e2
Caynan <caynanramos@gmail.com>
2022-10-07 18:57:29 -0300

    cards com totais

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { categories } from '../../utils/categories';
 import {
     Container,
     Title,
@@ -11,16 +12,11 @@ import {
     Date
 } from './styles';
 
-interface Category {
-    name: string;
-    icon: string;
-}
-
 export interface TransactionCardProps {
     type: 'up' | 'down';
     name: string;
     amount: string;
-    category: Category;
+    category: string;
     date: string;
 }
 
@@ -30,6 +26,11 @@ interface Props {
 }
 
 export function TransactionCard({ data }: Props) {
+    const category = categories.filter(
+        item => item.key === data.category
+    )[0]
+
+    console.log(category)
     return (
         <Container>
             <Title>{data.name}</Title>
@@ -39,8 +40,8 @@ export function TransactionCard({ data }: Props) {
             </Amount>
             <Footer>
                 <Category>
-                    <Icon name={data.category.icon} />
-                    <CategoryName>{data.category.name}</CategoryName>
+                    <Icon name={category.icon} />
+                    <CategoryName>{category.name}</CategoryName>
                 </Category>
                 <Date>{data.date}</Date>
             </Footer>
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { HighlightCard } from "../../components/HighlightCard";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
@@ -19,25 +20,43 @@ import {
     LogoutButton
 } from "./styles";
 
+
 export interface DatalistProps extends TransactionCardProps {
     id: string;
 }
 
+interface HighlightProps {
+    amount: string;
+}
+
+interface HighlightData {
+    entries: HighlightProps;
+    expensives: HighlightProps
+    total: HighlightProps
+}
+
 export function Dashboard() {
     const [data, setData] = useState<DatalistProps[]>([])
-
+    const [highlightData, setHighlightData] = useState<HighlightData>({} as HighlightData)
     async function loadTransactions() {
         const datakey = '@gofinances:trasactions';
         const response = await AsyncStorage.getItem(datakey);
         const transactions = response ? JSON.parse(response) : []
 
-        console.log(transactions)
+        let entriesTotal = 0
+        let expensiveTotal = 0
 
         const transactionsFormatted: DatalistProps[] = transactions.map((item: DatalistProps) => {
             const amount = Number(item.amount).toLocaleString('pt-BR', {
                 style: 'currency',
                 currency: 'BRL'
             })
+
+            if (item.type === 'up') {
+                entriesTotal += Number(item.amount)
+            } else {
+                expensiveTotal += Number(item.amount)
+            }
 
             return {
                 id: item.id,
@@ -50,18 +69,50 @@ export function Dashboard() {
         });
 
         setData(transactionsFormatted)
+        const total = entriesTotal - expensiveTotal
+        setHighlightData({
+            entries: {
+                amount: entriesTotal.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+            },
+            expensives: {
+                amount: expensiveTotal.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+            },
+            total: {
+                amount: total.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+            },
+
+        })
     }
 
     useEffect(() => {
         loadTransactions()
     }, [])
 
+    useFocusEffect(
+        useCallback(() => {
+            loadTransactions()
+        }, [])
+    )
+
     return (
         <Container>
             <Header>
                 <Userwrapper>
                     <UserInfo>
-                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/7688797?v=4' }} />
+                        <Photo
+                            source={{
+                                uri: 'https://avatars.githubusercontent.com/u/7688797?v=4'
+                            }}
+                        />
                         <User>
                             <UserGreeting>Olá,</UserGreeting>
                             <UserName>Caynan</UserName>
@@ -73,9 +124,24 @@ export function Dashboard() {
                 </Userwrapper>
             </Header>
             <HighlightCards>
-                <HighlightCard type="up" title="Entradas" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril" />
-                <HighlightCard type="down" title="Saídas" amount="- R$ 5.400,00" lastTransaction="Última saída dia 3 de abril" />
-                <HighlightCard type="total" title="Total" amount="R$ 5.400,00" lastTransaction="1 à 13 de abril" />
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount={highlightData?.entries?.amount}
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount={highlightData?.expensives?.amount}
+                    lastTransaction="Última saída dia 3 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount={highlightData?.total?.amount}
+                    lastTransaction="1 à 13 de abril"
+                />
             </HighlightCards>
 
             <Transactions>
